86818a4f619d57e7ec9562fe0fbea1780791a688
Chithlal <72548857+chith-lal@users.noreply.github.com>
2021-05-29 10:32:59 +0530

    Swipe Refresh layout added

--- app/src/main/java/com/chithlal/mobilestore/ui/activity/MainActivity.kt
+++ app/src/main/java/com/chithlal/mobilestore/ui/activity/MainActivity.kt
@@ -3,10 +3,14 @@ package com.chithlal.mobilestore.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.chithlal.mobilestore.R
+import com.chithlal.mobilestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
     }
 }
